27b9c6502380c2782c63ea45295f6587efb888bc
Arpit <arpit@revolution.ca>
2020-02-14 13:18:29 -0400

    added yargs pckg

--- app.js
+++ app.js
@@ -1,15 +1,10 @@
 const chalk = require('chalk')
-// const noteMessage = require('./notes')
+const noteMessage = require('./notes')
 
-// const message = noteMessage()
-// console.log(message)
+const command = process.argv[2]
 
-console.log(chalk.green('Hello world!'));
-
-const miles = 18;
-const calculateFeet = miles => miles * 5280;
- 
-console.log(chalk`
-    There are {bold 5280 feet} in a mile.
-    In {bold ${miles} miles}, there are {cyan.bgRed.bold ${calculateFeet(miles)} feet}.
-`);
+if(command === 'add') {
+    console.log("adding note")
+} else if(command === 'remove') {
+    console.log("removeign note")
+}
