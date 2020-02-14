f85927bc8136ca7d6123128aa7e48a2df264f16c
Arpit <arpit@revolution.ca>
2020-02-14 12:53:46 -0400

    added chalk pckg

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
-const validator = require('validator')
-const noteMessage = require('./notes')
+const chalk = require('chalk')
+// const noteMessage = require('./notes')
 
-const message = noteMessage()
-console.log(message)
+// const message = noteMessage()
+// console.log(message)
 
-console.log(validator.isURL('revolution.ca'))
+console.log(chalk.green('Hello world!'));
+
+const miles = 18;
+const calculateFeet = miles => miles * 5280;
+ 
+console.log(chalk`
+    There are {bold 5280 feet} in a mile.
+    In {bold ${miles} miles}, there are {cyan.bgRed.bold ${calculateFeet(miles)} feet}.
+`);
