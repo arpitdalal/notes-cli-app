96cca12057e81339a2562e789128fa43747aaa41
Arpit <arpit@revolution.ca>
2020-02-14 13:31:25 -0400

    added add, remove, lsit, and read terminal commands to be used by the user

--- app.js
+++ app.js
@@ -1,10 +1,46 @@
 const chalk = require('chalk')
+const yargs = require('yargs')
 const noteMessage = require('./notes')
 
-const command = process.argv[2]
+// Customize yargs version
+yargs.version('1.1.0')
 
-if(command === 'add') {
-    console.log("adding note")
-} else if(command === 'remove') {
-    console.log("removeign note")
-}
+// Create add command
+yargs.command({
+    command:    'add',
+    describe:   'Add a new note',
+    handler:    function() {
+        console.log('Adding a new note!')
+    }
+})
+
+// Create remove command
+yargs.command({
+    command:    'remove',
+    describe:   'Remove a new note',
+    handler:    function() {
+        console.log('Removing a new note!')
+    }
+})
+
+// Create list command
+yargs.command({
+    command:    'list',
+    describe:   'List the notes',
+    handler:    function() {
+        console.log('Listing out all notes!')
+    }
+})
+
+// Create read command
+yargs.command({
+    command:    'read',
+    describe:   'Read a note',
+    handler:    function() {
+        console.log('Reading a note!')
+    }
+})
+
+// add, remove, read, list
+
+console.log(yargs.argv)
